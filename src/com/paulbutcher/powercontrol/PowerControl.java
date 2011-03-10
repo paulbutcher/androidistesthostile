97f94a13965dd85952fc7e658b693e0a554bb105
Paul Butcher <paul@paulbutcher.com>
2011-03-10 12:22:04 +0000

    Add startImportant and stopImportant

--- src/com/paulbutcher/powercontrol/PowerControl.java
+++ src/com/paulbutcher/powercontrol/PowerControl.java
@@ -2,14 +2,27 @@ package com.paulbutcher.powercontrol;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.view.View;
 
 public class PowerControl extends Activity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    public void startImportant(View button) {
+        PowerManager powerManager = (PowerManager)getSystemService(POWER_SERVICE);
+        wakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK, "PowerControl");
+        wakeLock.acquire();
+    }
+    
+    public void stopImportant(View button) {
+        wakeLock.release();
+    }
+    
+    private PowerManager.WakeLock wakeLock;
 }
