b71207ca786089e1d6e148aed7b310a455a8cba3
Paul Butcher <paul@paulbutcher.com>
2011-03-12 11:16:31 +0000

    Add attempt4

--- test/src/com/paulbutcher/powercontrol/PowerControlTest.scala
+++ test/src/com/paulbutcher/powercontrol/PowerControlTest.scala
@@ -47,4 +47,19 @@ class PowerControlTest
     setActivityContext(testContext)
     startActivity(startIntent, null, null)
   }
+  
+  // def testAttempt4 {
+  //   val mockNewWakeLock = mockFunction[Int, String, PowerManager#WakeLock]
+  //   val mockPowerManager = new PowerManager {
+  //     override def newWakeLock(flags: Int, tag: String) = mockNewWakeLock(flags, tag)
+  //   }
+  //   val testContext = new ContextWrapper(getInstrumentation.getTargetContext) {
+  //     override def getSystemService(name: String) = name match {
+  //       case "power" => mockPowerManager
+  //       case _ => super.getSystemService(name)
+  //     }
+  //   }
+  //   setActivityContext(testContext)
+  //   startActivity(startIntent, null, null)
+  // }
 }
